879fe2b107c434ffc191bbde578c32884c24e96e
pranav-ap <pranavap96@gmail.com>
2018-03-28 18:57:40 +0530

    cleaning almost over

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -2,7 +2,11 @@ import pandas as pd
 import numpy as np
 
 from sklearn.preprocessing import LabelEncoder
-labelencoder = LabelEncoder()
+label_encoder = LabelEncoder()
+
+from sklearn.preprocessing import StandardScaler
+standard_scaler = StandardScaler()
+
 
 # get original datasets
 train = pd.read_csv('./data/train.csv')
@@ -10,32 +14,53 @@ test = pd.read_csv('./data/test.csv')
 
 datasets = [ train, test ]
 
+
 # Fill Nulls
 for dataset in datasets:    
     #complete missing age with median
     dataset['Age'].fillna(dataset['Age'].median(), inplace = True)
 
+
 # Drop unnecessary features
-columns_to_drop = ['PassengerId', 'Ticket', 'Fare', 'Cabin', 'Embarked']
+columns_to_drop = ['PassengerId', 'Ticket', 'Name', 'Fare', 'Cabin', 'Embarked']
 
 for dataset in datasets:
-    dataset.drop(
-        columns_to_drop, 
-        axis = 1, 
-        inplace = True)
-
-    dataset.info()
+    dataset.drop(columns_to_drop, axis = 1, inplace = True)
 
 
 # create family size
+for dataset in datasets:
+    dataset["Family_Size"] = dataset['SibSp'] + dataset['Parch'] + 1
+    dataset["Family_Size"].head()
 
-# drop siblings, parents features
 
-# create age
-    
+# drop no of siblings, parents, children and spouses features
+columns_to_drop = ['SibSp', 'Parch']
+
+for dataset in datasets:
+    dataset.drop(columns_to_drop, axis = 1, inplace = True)
+ 
 # encode gender
-    
+for dataset in datasets:
+    dataset[ 'Sex' ] = label_encoder.fit_transform(dataset[ 'Sex' ])
+
+
+train.info()
+
+# split independent and dependent features
+X_train = train.drop([ 'Survived' ], axis = 1)
+y_train = train['Survived']
+
+X_test = test
+
+
 # scale the features
+X_train = standard_scaler.fit_transform(X_train)
+X_test = standard_scaler.transform(X_train)
+
+X_train.info()
+#for dataset in [ X_train, X_test ]:
+ #   dataset.info()
 
 # save as csv
 
